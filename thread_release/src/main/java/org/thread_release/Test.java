273e8ef20a0144df54ed8aebcfff2c63aacea50b
serbinav <serbinav86@gmail.com>
2016-03-13 00:23:05 +0300

    start thread

--- thread_release/src/main/java/org/thread_release/Test.java
+++ thread_release/src/main/java/org/thread_release/Test.java
@@ -1,14 +1,10 @@
 package org.thread_release;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.console_release.StorageInCollectionThread;
-import org.console_release.StorageInList;
-import org.printing_module.Ingredient;
-import org.printing_module.Person;
-import org.printing_module.ProductionPizza;
-import org.printing_module.Storage;
+
+import org.printing_module.Kitchener;
+
 import org.printing_module.Task;
 
 public class Test {
@@ -16,28 +12,10 @@ public class Test {
 	public static void main(String[] args) {
 			
 		List<Task> listTask;
-
-		Storage newStorage = new StorageInList();
-		List<Ingredient> listComponent = new ArrayList<>();
-
-		listComponent.add(new Ingredient("сыр", 100));
-		listComponent.add(new Ingredient("колбаса", 100));
-		listComponent.add(new Ingredient("перец", 100));
-		newStorage.setListComponent(listComponent);
 		
-    	Person tmpPerson = new Person();
-    	
-		ProductionPizza once = new StorageInCollectionThread();
-		int sizePizza = once.selectPizzaSize();
-		while (sizePizza <= 0 || sizePizza > 4100) {
-			sizePizza = once.selectPizzaSize();
-		}
+		Kitchener cooker;
 		
-		tmpPerson.getSinglePizza().setSizePizza(sizePizza);
+		Thread myThready = new Thread();
 
-		tmpPerson.getSinglePizza().setListIngredients(once.chooseIngredientForPizza(newStorage));
-
-		once.printOrderPizza(tmpPerson.getSinglePizza());
-       	
 	}
 }
