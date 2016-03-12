80d268b64c4e3938f108f32c235bbe25e6feda01
serbinav <serbinav86@gmail.com>
2016-03-13 00:47:41 +0300

    new class

--- thread_release/src/main/java/org/thread_release/Test.java
+++ thread_release/src/main/java/org/thread_release/Test.java
@@ -1,19 +1,35 @@
 package org.thread_release;
 
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
-import org.printing_module.Ingredient;
 import org.printing_module.Kitchener;
 
 import org.printing_module.Task;
 
+class TasQueue 
+{ 
+	private List<Task> tasks = new LinkedList<>();
+
+	public List<Task> getTasks() {
+		return Collections.unmodifiableList(tasks);
+	}
+
+	public void setTasks(List<Task> tasks) {
+		this.tasks = tasks;
+	}
+	
+	public int size() {
+		return tasks.size();
+	}
+}
+
 public class Test {
-
 	public static void main(String[] args) {
-			
-		List<Task> listTask = new ArrayList<>();
-
+		
+		TasQueue cookQueue = new TasQueue();
+		
 		Kitchener cooker = new Kitchener();
 		
 		Thread cookThread = new Thread();
