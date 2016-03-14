03b8da08ce2f704f7d4687865268fe66e80a41ad
serbinav <serbinav86@gmail.com>
2016-03-14 23:37:38 +0300

    add task in Queue

--- thread_release/src/main/java/org/thread_release/KitchenerRunnable.java
+++ thread_release/src/main/java/org/thread_release/KitchenerRunnable.java
@@ -1,18 +1,23 @@
 package org.thread_release;
 
 import org.printing_module.Kitchener;
+import org.printing_module.Person;
 import org.printing_module.Storage;
 
 public class KitchenerRunnable implements Runnable{
 	
 	TaskQueue cookQueue;
 	Kitchener cooker;
+	Person client;
 	Storage newStorage;
 
 	public KitchenerRunnable(TaskQueue cookQueue, Storage newStorage) {
 		this.cooker = new Kitchener();
+		this.client = new Person();
 		this.cookQueue = cookQueue;
 		this.newStorage = newStorage;
+		
+		this.cookQueue.put(this.client.createTask());
 	}
 
 	@Override
